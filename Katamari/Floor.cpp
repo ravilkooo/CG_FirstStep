821681327322406cdca27c0ec594eb19d5f00b2f
Ravil <ravilka.zftsh@yandex.ru>
2025-03-23 18:59:54 +0300

    Added FloorShader

--- Katamari/Floor.cpp
+++ Katamari/Floor.cpp
@@ -49,7 +49,7 @@ Floor::Floor(ID3D11Device* device)
 			D3D11_INPUT_PER_VERTEX_DATA,
 			0 };
 
-	shaderFilePath = L"./Shaders/PlaneShader.hlsl";
+	shaderFilePath = L"./Shaders/FloorShader.hlsl";
 	this->textures.push_back(Texture(device, "models\\Textures\\carpet.dds", aiTextureType_DIFFUSE));	
 	hasTexture = true;
 }
